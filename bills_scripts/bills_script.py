cfa577a39a539023c68a0d2027854f3d66ac76c2
Dylan Price <dylan@cs.washington.edu>
2011-01-02 14:07:28 -0800

    Bills script done.

--- bills_scripts/bills_script.py
+++ bills_scripts/bills_script.py
@@ -1,16 +1,79 @@
 #!/usr/bin/python
 
 # Bills Script
-# Last edited: 01/01/2011
+# Last edited: 01/02/2011
 
 # Parses bills.txt and sends e-mails to people who need to pay bills.
 
 import bills_yacc
 import sys
+import config
+from datetime import date
+from smtplib import SMTP
+from email.MIMEText import MIMEText
+from base64 import b64decode
+
+class SmtpLoggedInConnection:
+
+    def __init__(self, smtpserver, username, password, debuglevel):
+        self.smtpserver = smtpserver
+        self.username = username
+        self.password = password
+        self.debuglevel = debuglevel
+
+    def __enter__(self):
+        self.conn = SMTP(self.smtpserver)
+        self.conn.set_debuglevel(self.debuglevel)
+        self.conn.starttls()
+        self.conn.login(self.username, self.password)
+        return self.conn
+
+    def __exit__(self, type, value, traceback):
+        self.conn.close()
+        return True
+
+def extract_owestrings(parse):
+    emails = {}
+    for bill in parse:
+        timediff = bill['due_date'] - date.today()
+        if timediff.days <= config.REMIND_INTERVAL:
+            num_names = len(bill['names'])
+            for name in bill['names'].keys():
+                amount = bill['names'][name][0]
+                if amount == -1:
+                    amount = bill['amount'] / num_names
+                paid = bill['names'][name][1]
+                if not paid:
+                    if not name in emails:
+                        emails[name] = ""
+                    emails[name] += config.OWESTRING % \
+                        (amount, bill['bill'], bill['due_date'].strftime("%A, %B %d"))
+    return emails
+
+def send_emails(emails):
+    with SmtpLoggedInConnection(config.SMTP_SERVER, config.SMTP_USERNAME, \
+                                b64decode(config.SMTP_PASSWORD), config.DEBUG_LEVEL) as conn:
+        for name in emails.keys():
+            send_email(conn, config.EMAILS['Dylan'], config.EMAILS[name], \
+                       config.SUBJECT, config.MSG % (name, emails[name]))
+
+def send_email(conn, sender, receiver, subject, msg):
+    msg = MIMEText(msg, 'plain')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    conn.sendmail(sender, [receiver], msg.as_string())
 
 def main():
-    billsfile = open(sys.argv[1], 'r')
-    parse = bills_yacc.parse(billsfile.read())
+    try:
+        billsfile = open(sys.argv[1], 'r')
+        parse = bills_yacc.parse(billsfile.read())
+        emails = extract_owestrings(parse)
+        send_emails(emails)
+    except Exception as exc:
+        with SmtpLoggedInConnection(config.SMTP_SERVER, config.SMTP_USERNAME, \
+                                    b64decode(config.SMTP_PASSWORD), config.DEBUG_LEVEL) as conn:
+            send_email(conn, config.EMAILS['Dylan'], config.EMAILS['Dylan'], \
+                       config.ERRMSG_SUBJECT, config.ERRMSG % exc)
 
 if __name__ == "__main__":
     main()
---
+++ bills_scripts/config.py
@@ -0,0 +1,38 @@
+# If a bill is due within REMIND_INTERVAL days, then an email will be sent
+REMIND_INTERVAL = 7
+
+# A dictionary from names to email addresses
+EMAILS = {
+}
+
+# SMTP server config
+# SMTP_PASSWORD is base64 encoded
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_USERNAME = 
+SMTP_PASSWORD = 
+
+SUBJECT = 'Bills'
+# Parameters are (name, formatted_owestring)
+MSG = '''\
+Hey %s,
+
+You owe:
+%s
+Thanks,
+'''
+# Parameters are (amount, billname, due_date_string)
+OWESTRING = "    $%.2f for %s which is due on %s \n"
+
+ERRMSG_SUBJECT = 'Error in bills script!'
+# Parameters are (error)
+ERRMSG = '''\
+Error:
+    %s
+'''
+
+# Set to true to print smtp debug info
+DEBUG_LEVEL = False
+
+# Uncomment when debugging to prevent sending out emails to people
+#EMAILS = {
+#}
