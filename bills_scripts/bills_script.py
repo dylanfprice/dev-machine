446e5adb03939e87bdb811b9ebc387e5ca329426
Dylan Price <dylan@dylan-minitower.doesntexist.org>
2011-02-10 18:33:34 -0800

    Added logging and backup of bills.txt file.

--- bills_scripts/bills_script.py
+++ bills_scripts/bills_script.py
@@ -7,6 +7,7 @@
 
 import bills_yacc
 import sys
+import shutil
 import config
 from datetime import date
 from smtplib import SMTP
@@ -63,19 +64,34 @@ def send_email(conn, sender, receiver, subject, msg):
     msg['From'] = sender
     conn.sendmail(sender, [receiver], msg.as_string())
 
+def log(logfile, logmessage):
+    with open(logfile, 'a+') as file:
+        file.write(logmessage + "\n")
+
+def backup(billsfile):
+    shutil.copyfile(billsfile, billsfile + ".backup")
+
 def main():
+    logmessage = None
     try:
         if len(sys.argv) < 2:
+            logmessage = "Did not receive a file argument."
             raise Exception("Did not receive a file argument.")
         billsfile = open(sys.argv[1], 'r')
+        backup(sys.argv[1])
         parse = bills_yacc.parse(billsfile.read())
         emails = extract_owestrings(parse)
         send_emails(emails)
+        logmessage = "[%s] Success!" % date.today()
     except Exception as exc:
         with SmtpLoggedInConnection(config.SMTP_SERVER, config.SMTP_USERNAME, \
                                     b64decode(config.SMTP_PASSWORD), config.DEBUG_LEVEL) as conn:
             send_email(conn, config.EMAILS['Dylan'], config.EMAILS['Dylan'], \
                        config.ERRMSG_SUBJECT, config.ERRMSG % exc)
+            logmessage = "[%s] Failure. %s \n %s" % (date.today(), parse, emails)
+
+    if logmessage:
+        log(config.LOGFILE, logmessage)
 
 if __name__ == "__main__":
     main()
