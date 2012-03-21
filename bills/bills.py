7ad281aad56e4f84156bd9b1c02c316a59b45871
Dylan Price <the.dylan.price@gmail.com>
2012-03-21 02:57:58 -0700

    Most things working now.

--- bills/bills.py
+++ bills/bills.py
@@ -1,7 +1,8 @@
+import cgi
+import config
+import datetime
 import jinja2
 import os
-import cgi
-import datetime
 import urllib
 import webapp2
 
@@ -13,30 +14,46 @@ from google.appengine.api import users
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-BILL_KEY = 'bill'
+BILLS_KEY = 'bills'
 
-class SeeBill(webapp2.RequestHandler):
+class SeeBills(webapp2.RequestHandler):
     def get(self):
-        bill = Bill.get_or_insert(BILL_KEY)
-        template_values = {'bill': bill}
+        bills = Bills.get_or_insert(BILLS_KEY)
+        template_values = {'bills': bills}
 
-        template = jinja_env.get_template('templates/index.html')
+        template = jinja_env.get_template('templates/see_bills.html')
         self.response.out.write(template.render(template_values))
 
-class EditBill(webapp2.RequestHandler):
+class EditBills(webapp2.RequestHandler):
     def get(self):
-        bill = Bill.get_or_insert(BILL_KEY)
-        template_values = {'bill': bill}
+        bills = Bills.get_or_insert(BILLS_KEY)
+        template_values = {'bills': bills}
 
-        template = jinja_env.get_template('templates/edit_bill.html')
+        template = jinja_env.get_template('templates/edit_bills.html')
         self.response.out.write(template.render(template_values))
 
     def post(self):
-        bill = Bill.get_or_insert(BILL_KEY)
-        bill.content = self.request.get('content')
-        bill.put()
+        bills = Bills.get_or_insert(BILLS_KEY)
+        bills.content = self.request.get('content')
+        bills.put()
         self.redirect('/')
 
-app = webapp2.WSGIApplication([('/', SeeBill),
-                               ('/edit', EditBill)],
+class Total(webapp2.RequestHandler):
+    def get(self, name):
+        bills = Bills.get_or_insert(BILLS_KEY)
+        template_values = {
+            'name' : name,
+            'greeting' : '',
+            'remind_interval': config.REMIND_INTERVAL,
+            'host_url' : self.request.host_url,
+            'bills': bills
+        }
+
+        template = jinja_env.get_template('templates/total_page.html')
+        self.response.out.write(template.render(template_values))
+        
+app = webapp2.WSGIApplication([('/', SeeBills),
+                               ('/edit', EditBills),
+                               (r'/total', Total),
+                               (r'/total/(.*)', Total)],
                               debug=True)
---
+++ bills/config.py
@@ -0,0 +1,14 @@
+# If a bill is due within REMIND_INTERVAL days, then an email will be sent
+REMIND_INTERVAL = 21
+
+# A dictionary from names to email addresses
+EMAILS = {
+}
+
+# SMTP server config
+# SMTP_PASSWORD is base64 encoded
+#SMTP_SERVER = 'smtp.gmail.com'
+#SMTP_USERNAME = 
+#SMTP_PASSWORD = 
+
+SUBJECT = 'Bills'
