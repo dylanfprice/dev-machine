a35625e37392267bad3dfac67179871d022d1f32
Dylan Price <the.dylan.price@gmail.com>
2013-03-11 19:51:31 -0700

    Email program for janelle.

---
+++ skyfest/msg.py
@@ -0,0 +1,50 @@
+SUBJECT="Sky Fest - {company}"
+TEXT = """\
+Hi,
+
+
+My name is Janelle Deane and the event planner for the UW Kayak Club and Outdoor Adventure Center. I'm not sure if this is the proper department to contact, but I was wondering if {company} would be willing to work with our festival. This July 4th-7th we are hosting a four day outdoor recreational festival for the greater Seattle area, named Sky Fest. This competition will bring together outdoor enthusiasts  and families. We are also using the event as a fundraiser for American Whitewater, Access Fund, and Mt Bike Alliance, all non-profits protecting our nation's recreation. The main events are 4 downriver races, a 2 day climbing competition, a 4 day vertical mt bike challenge and 2 charity dashes. We also have many clinics and workshops for people of all skill levels.
+
+
+We expect an attendance of over 4,000 people at our event. This would be a great opportunity for {company} to advertise to kayakers, climbers, fishermen, runners, mt bikers and to a broad audience of outdoor enthusiasts. We received $60k to help with the advertising of our event so sponsors' names and logos will be displayed on all event advertisements in magazines, event flyers, event banners, and the event website. They will also be recognized during award ceremonies. At the moment we are looking for gear donations for prizes, title sponsors and on-site vendors but we would greatly appreciate any type of sponsorship.
+
+
+Please let me know what you think, and don't hesitate to get in touch if you want to know more about the festival. Our website is: http://www.skyfest.org/
+
+Thank you!
+
+Janelle Deane
+
+Event Director
+University Kayak Club at the UW and Outdoor Adventure Center 
+425-681-7484
+"""
+
+HTML = """\
+<html>
+  <head></head>
+  <body>
+    Hi,<br>
+
+    <p>
+    My name is Janelle Deane and the event planner for the UW Kayak Club and Outdoor Adventure Center. I'm not sure if this is the proper department to contact, but I was wondering if {company} would be willing to work with our festival. This July 4th-7th we are hosting a four day outdoor recreational festival for the greater Seattle area, named Sky Fest. This competition will bring together outdoor enthusiasts  and families. We are also using the event as a fundraiser for American Whitewater, Access Fund, and Mt Bike Alliance, all non-profits protecting our nation's recreation. The main events are 4 downriver races, a 2 day climbing competition, a 4 day vertical mt bike challenge and 2 charity dashes. We also have many clinics and workshops for people of all skill levels.
+    </p>
+
+    <p>
+    We expect an attendance of over 4,000 people at our event. This would be a great opportunity for {company} to advertise to kayakers, climbers, fishermen, runners, mt bikers and to a broad audience of outdoor enthusiasts. We received $60k to help with the advertising of our event so sponsors' names and logos will be displayed on all event advertisements in magazines, event flyers, event banners, and the event website. They will also be recognized during award ceremonies. At the moment we are looking for gear donations for prizes, title sponsors and on-site vendors but we would greatly appreciate any type of sponsorship.
+    </p>
+
+    <p>
+    Please let me know what you think, and don't hesitate to get in touch if you want to know more about the festival. Our website is: <a href="http://www.skyfest.org">http://www.skyfest.org/</a>
+    </p>
+
+    Thank you!<br>
+
+    Janelle Deane<br>
+
+    Event Director<br>
+    University Kayak Club at the UW and Outdoor Adventure Center <br>
+    425-681-7484<br>
+  </body>
+</html>
+"""
