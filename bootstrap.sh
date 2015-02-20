ec77ab03dc4d1e22658f137e032a9349b0ccc537
Dylan Price <the.dylan.price@gmail.com>
2015-02-19 20:34:32 -0800

    Added vim role and updated bootstrap to echo manual preconditions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo 'installing ansible'
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
+
+echo 'make sure you have an ssh key, and pushed it to github and bitbucket'
