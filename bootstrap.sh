d8bd822da96993ca903a8feb8675bb192022da21
Dylan Price <the.dylan.price@gmail.com>
2015-07-17 10:10:48 -0700

    Add extra newline to make ssh key line more apparent

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,4 @@ sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
 
-echo 'make sure you have an ssh key, and pushed it to github and bitbucket'
+echo '\nmake sure you have an ssh key, and pushed it to github and bitbucket'
