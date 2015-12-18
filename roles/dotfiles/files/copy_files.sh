ed7a00bb74aba613051a7a0a675df23b9b5cf779
Dylan Price <the.dylan.price@gmail.com>
2015-12-17 16:37:39 -0800

    Make alias for running tox with custom index.

--- roles/dotfiles/files/copy_files.sh
+++ roles/dotfiles/files/copy_files.sh
@@ -7,5 +7,4 @@ mkdir -p $ENERGYSAVVY_DIR/config_files/
 devstack cp /etc/pip.conf /vagrant/config_files/
 devstack cp /home/django/.config/flake8 /vagrant/config_files/
 
-cp $ENERGYSAVVY_DIR/config_files/pip.conf ${ENERGYSAVVY_VENV}/pip.conf
 cp $ENERGYSAVVY_DIR/config_files/flake8 $HOME/.config/flake8
