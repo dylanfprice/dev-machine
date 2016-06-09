7c8058479d6bb40cdafd2907121d19b031e49a35
Dylan Price <the.dylan.price@gmail.com>
2016-06-08 18:01:39 -0700

    Add eastwood to user dependencies.

--- roles/dotfiles/files/profiles.clj
+++ roles/dotfiles/files/profiles.clj
@@ -1,1 +1,2 @@
-{:user {:plugins [[cider/cider-nrepl "0.12.0"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.12.0"]]
+        :dependencies [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]]}}
