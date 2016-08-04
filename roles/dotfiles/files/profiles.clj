1d9f1c8c96c937d537b1bf7d49a395b62a0fec0a
Dylan Price <the.dylan.price@gmail.com>
2016-08-03 17:13:55 -0700

    Update cider-nrepl.

--- roles/dotfiles/files/profiles.clj
+++ roles/dotfiles/files/profiles.clj
@@ -1,2 +1,2 @@
-{:user {:plugins [[cider/cider-nrepl "0.12.0"]]
+{:user {:plugins [[cider/cider-nrepl "0.13.0"]]
         :dependencies [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]]}}
