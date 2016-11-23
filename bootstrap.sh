4cfda9044cc6231e35b056a7f74747d2d8b9c129
Dylan Price <the.dylan.price@gmail.com>
2016-11-22 23:23:59 -0800

    Remove firefox add-on role and put add-ons in bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,19 @@
-#!/bin/bash
+#!/bin/bash 
 
 echo 'installing ansible'
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
-sudo ansible-galaxy install -r requirements.yml
 
-echo '\nmake sure you have an ssh key, and pushed it to github and bitbucket'
+echo '\nmake sure you have an ssh key, and pushed it to github and bitbucket' 
+read -p 
+
+echo 'install the following firefox add-ons:'
+echo 'https://addons.mozilla.org/en-US/firefox/addon/vimperator/'
+echo 'https://addons.mozilla.org/en-US/firefox/addon/tree-style-tab/'
+echo 'https://addons.mozilla.org/en-US/firefox/addon/session-manager/'
+echo 'https://addons.mozilla.org/en-US/firefox/addon/privacy-badger-firefox/'
+echo 'https://addons.mozilla.org/en-US/firefox/addon/lastpass-password-manager/'
+echo 'https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/'
+echo 'https://addons.mozilla.org/en-US/firefox/addon/decentraleyes/'
+read -p
