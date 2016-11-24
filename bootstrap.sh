f93eead7ecc2fc0f968ec9d3d08023bf08e38f8b
Dylan Price <the.dylan.price@gmail.com>
2016-11-23 16:50:37 -0800

    Updated privacy badger url.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,11 +8,14 @@ sudo apt-get install ansible
 echo '\nmake sure you have an ssh key, and pushed it to github and bitbucket' 
 read
 
+echo 'create a new gnome terminal profile with the name "Default"'
+read
+
 echo 'install the following firefox add-ons:'
 echo 'https://addons.mozilla.org/en-US/firefox/addon/vimperator/'
 echo 'https://addons.mozilla.org/en-US/firefox/addon/tree-style-tab/'
 echo 'https://addons.mozilla.org/en-US/firefox/addon/session-manager/'
-echo 'https://addons.mozilla.org/en-US/firefox/addon/privacy-badger-firefox/'
+echo 'https://addons.mozilla.org/en-US/firefox/addon/privacy-badger17/'
 echo 'https://addons.mozilla.org/en-US/firefox/addon/lastpass-password-manager/'
 echo 'https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/'
 echo 'https://addons.mozilla.org/en-US/firefox/addon/decentraleyes/'
