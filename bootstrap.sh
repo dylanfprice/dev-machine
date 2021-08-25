0d769c469e29f89eb5569e1c5edb11bbf8a65493
Dylan Price <dylan.price@duke.edu>
2021-08-25 14:27:11 -0700

    Bring bootstrap script a little more in line with what actually happens now.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,17 @@ echo 'disabling graphical boot'
 sudo systemctl disable display-manager.service
 sudo systemctl set-default multi-user.target
 
-echo 'installing ansible'
+echo 'get an internet connection!'
+echo '(might need to plug in to ethernet and set up systemd-networkd)'
+read
+
+echo 'installing sudo'
 sudo apt update
+sudo apt install sudo
+echo '\nadd me to sudoers group'
+read
+
+echo 'installing ansible'
 sudo apt install ansible
 
 echo '\nmake sure you have an ssh key, and pushed it to github and gitlab' 
@@ -14,4 +23,4 @@ read
 echo 'log in to firefox account and sync add-ons'
 read
 
-echo '*after* running the playbook, run `dropbox status`'
+echo '*after* running the playbook, run `dropbox update`'
